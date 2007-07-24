dc3225ea8a9eb06b8c73fc791cf5a3b22fa88c08
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-07-24 13:24:10 +0000

    Added GPLv3 licensing

--- tools/video_writer.c
+++ tools/video_writer.c
@@ -1,4 +1,22 @@
 /* build like this: gcc video_writer.c -I /usr/include/opencv -lcv -lhighgui */
+/* Video Writer - tiny program for creating videos out of a list of images, using OpenCV */
+/* Copyright 2007, Noam Lewis, enoughmail@googlegroups.com */
+/*
+    This file is part of Enough.
+
+    Enough is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Enough is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
