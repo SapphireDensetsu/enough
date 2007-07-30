ba36c6e61f3112afb56dccf81f8c207d1b284302
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-07-30 01:53:21 +0000

    Scancodes in its own file

--- graphui/GraphApp.py
+++ graphui/GraphApp.py
@@ -31,34 +31,7 @@ from Lib.Point import Point
 
 from guilib import get_default, MovingLine, paint_arrowhead_by_direction, pygame_reverse_key_map, rotate_surface, point_near_polyline
 
-scancode_map = {
-    24 : pygame.K_q,
-    25 : pygame.K_w,
-    26 : pygame.K_e,
-    27 : pygame.K_r,
-    28 : pygame.K_t,
-    29 : pygame.K_y,
-    30 : pygame.K_u,
-    31 : pygame.K_i,
-    32 : pygame.K_o,
-    33 : pygame.K_p,
-    38 : pygame.K_a,
-    39 : pygame.K_s,
-    40 : pygame.K_d,
-    41 : pygame.K_f,
-    42 : pygame.K_g,
-    43 : pygame.K_h,
-    44 : pygame.K_j,
-    45 : pygame.K_k,
-    46 : pygame.K_l,
-    52 : pygame.K_z,
-    53 : pygame.K_x,
-    54 : pygame.K_c,
-    55 : pygame.K_v,
-    56 : pygame.K_b,
-    57 : pygame.K_n,
-    58 : pygame.K_m,
-}
+from scancodes import scancode_map
 
 class NodeValue(object):
     def __init__(self, name, group_name = None, start_pos=None):
