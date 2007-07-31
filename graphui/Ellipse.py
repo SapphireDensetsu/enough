6cb6417162f81fb853379072393620d5c7d20134
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-07-31 18:10:48 +0000

    testing alternative intersections code

--- graphui/Ellipse.py
+++ graphui/Ellipse.py
@@ -1,63 +1,97 @@
 from __future__ import division
 
 import math
-from Lib.Point import Point
+from Lib.Point import Point, VectorsNotColinear
+
+def find_vect_from_point_to_line(point, src, dest):
+    # src and dest are two points on the line
+    # Finds the vector pointing from point to the nearest point on the line
+    u = src
+    v = dest - src
+    w = point
+    t = (((u-w).dot_product(v))/(v.dot_product(v)))
+    e = u - w - v * t
+    return e
+
+    
+def point_in_line_segment(point, src, dest, delta=0.1**15):
+    if src == dest:
+        raise ValueError("Line can't be defined by a single point")
+    v = dest - src
+    u = point - src
+    try:
+        t = u.find_linear_coefficient(v)
+    except VectorsNotColinear:
+        return False
+    if t is None or (t >= 0 and t <= 1):
+        return True
+    return False
+
+
+class Circle(object):
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+
+    def intersections(self, src, dest, delta=0.1**15):
+        if src == dest:
+            raise ValueError("Line can't be defined by a single point")
+        v = (dest-src)
+        e = find_vect_from_point_to_line(self.center, src, dest)
+        d = self.radius**2 - e.dot_product(e)
+        if d < 0:
+            # The distance of the line from center of circle, is
+            # greater than radius.  The line does not intersect.
+            return tuple()
+        b1 = v * (math.sqrt(d) / v.norm())
+        p1 =  b1 + self.center + e
+        p2 = -b1 + self.center + e
+        res = [point for point in (p1, p2)
+               if point_in_line_segment(point, src,dest)]
+        return tuple(res)
+
+_unit_circle = Circle(Point(0,0), 1)
 
 class Ellipse(object):
     def __init__(self, rect):
         self.rect = rect
         
-    def intersections(self, src, dest):
+    def intersections(self, src, dest, delta=0.1**15):
         """Returns the point of the intersection between the infinite
         line defined by (src, dest) and this shape, or None if there
         is no such intersection"""
         if src == dest:
-            raise ValueError("Line is not defined by a single point")
-        cx, cy = self.rect.center
-        w, h = self.rect.size
-        x1, y1 = src.x, src.y
-        x2, y2 = dest.x, dest.y
-        if x1==x2:
-            # x = my + n
-            m = 1. * (x2-x1)/(y2-y1)
-            n = 1. * (x1*y2-x2*y1)/(y2-y1)
-
-            s = (-4.*(n**2)-8*cy*m*n+8*cx*n + (h**2-4*(cy**2))*(m**2) + 8*cx*cy*m + (w**2)-4*(cx**2))
-            if s < 0:
-                return
-            sq = math.sqrt(s)*h*w
-            div = (2.*(h**2)*(m**2)+2*(w**2))
-
-            xexpr = 2.*(w**2)*n+2*cx*(h**2)*(m**2)+2*cy*(w**2)*m
-            yexpr = 2.*(h**2)*m*n-2*cx*(h**2)*m-2*cy*(w**2)
+            raise ValueError("Line can't be defined by a single point")
+        if self.rect.width == 0 or self.rect.height == 0:
+            return tuple()
+        
+        # Squish the line to reflect intersections on us if we were a unit circle.
+        c = Point(*self.rect.center)
+        def squish_point(p):
+            tp = p - c
+            return Point(tp.x/self.rect.width*2, tp.y/self.rect.height*2)
+        def unsquish_point(p):
+            return Point(p.x*self.rect.width/2, p.y*self.rect.height/2)  + c
             
-            i1 = Point(-(m*sq-xexpr)/div, -(sq+yexpr)/div)
-            i2 = Point(-(-m*sq-xexpr)/div, -(-sq+yexpr)/div)
-        else:
-            # y = mx + n
-            m = (y2-y1)/(x2-x1)
-            n = (x2*y1-x1*y2)/(x2-x1)
+        nsrc = squish_point(src)
+        ndest = squish_point(dest)
+        res = _unit_circle.intersections(nsrc, ndest, delta=delta)
+        return tuple(unsquish_point(p) for p in res)
 
-            s = (-4*(n**2)-8*cx*m*n+8*cy*n+(w**2)*(m**2)-4*(cx**2)*(m**2)+8*cx*cy*m+(h**2)-4*(cy**2))
-            if s < 0:
-                return
-            sq = math.sqrt(s)*h*w
-            div = (2.*(w**2)*(m**2)+2*(h**2))
-            xexpr = 2.*(w**2)*m*n-2*cy*(w**2)*m-2*cx*(h**2)
-            yexpr1 = 2.*cx*(h**2)
-            yexpr2 = 2.*(h**2)*n+2*cy*(w**2)*(m**2)
-            i1 = Point(-(sq+xexpr)/div, (m*(yexpr1-sq)+yexpr2)/div)
-            i2 = Point(-(-sq+xexpr)/div,(m*(yexpr1+sq)+yexpr2)/div)
 
-        margin_error = 0.0001
-        for i in [i1, i2]:
-            if (((x1-margin_error <= i.x <= x2+margin_error) or
-                 (x2-margin_error <= i.x <= x1+margin_error)) and
-                ((y1-margin_error <= i.y <= y2+margin_error)) or
-                (y2-margin_error <= i.y <= y1+margin_error)):
-                yield i
 
-    # Dead code, delete:?
-    def inside(self, p):
-        cx, cy = self.rect.center
-        return ((2.*(p.x-cx)/self.rect.width)**2 + (2.*(p.y-cy)/self.rect.height)**2 <= 1)
+def _test_speed(num=10):
+    import pygame
+    def rand_point(n=5):
+        return Point(random.random()*n*2-n, random.random()*n*2-n)
+    es = []
+    for i in xrange(200):
+        es.append(Ellipse(pygame.Rect(rand_point().as_tuple(), rand_point().as_tuple())))
+
+    import time
+    t = time.time()
+    for i in xrange(num):
+        for e in es:
+            e.intersections(rand_point(10), rand_point(10))
+    print 'Time:', time.time() - t, ',', num*i, 'intersections'
+        
