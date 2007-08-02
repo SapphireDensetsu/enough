f9231e926e26ecd4364fdf08d0585a376c824446
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-08-02 01:05:51 +0000

    Trying to optimize Ellipse by creating a .pyx

--- graphui/setup.py
+++ graphui/setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup, Extension
 
 if __name__ == '__main__':
     from Pyrex.Compiler.Main import compile, default_options
-    for pyrex_source in ['Lib/Point.pyx']:
+    for pyrex_source in ['Lib/Point.pyx', 'Ellipse.pyx']:
         print "Compiling", pyrex_source
         compile(pyrex_source, default_options)
 
-    setup(ext_modules=[Extension("Lib.Point", ["Lib/Point.c"])])
+    setup(ext_modules=[Extension("Lib.Point", ["Lib/Point.c"]),
+                       Extension("Ellipse", ["Ellipse.c"])])
