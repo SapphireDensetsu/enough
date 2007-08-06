cad8201cb8db4e99977ae5eebc187e8678ee4452
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-08-06 09:41:24 +0000

    minor changes - cached some funcs

--- graphui/Lib/Bezier.py
+++ graphui/Lib/Bezier.py
@@ -58,14 +58,14 @@ def rec_point_on_curve(cp, t):
 
 from Func import cached
 
-#@cached
+@cached
 def factorial(n):
     r = 1
     for i in xrange(2,n+1):
         r *= i
     return r
 
-#@cached
+@cached
 def choose(k, n):
     assert k <= n, "K must be < N"
     return factorial(n)/(factorial(k)*factorial(n-k))
