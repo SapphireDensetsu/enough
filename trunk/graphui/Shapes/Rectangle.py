7bc2aa6df87a2de4ba2a9619fb95279a6093cb92
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-08-07 22:42:21 +0000

    GraphWidget color fixes

--- trunk/graphui/Shapes/Rectangle.py
+++ trunk/graphui/Shapes/Rectangle.py
@@ -1,17 +1,19 @@
+import pygame
+
 class Rectangle(object):
     def __init__(self, rect):
         self.rect = rect
 
     def paint(self, offset, surface, fore_color, back_color):
-        import pygame
         rect = pygame.Rect(self.rect.x+offset.x, self.rect.y+offset.y, self.rect.w, self.rect.h)
-        
-        rect = rect.clip(surface.get_rect())
-        subsurf = surface.subsurface(rect)
-        # This is MUCH faster than draw.rect
-        subsurf.fill(back_color)
-        
-        pygame.draw.rect(surface, fore_color, rect, 2)
+        if back_color:
+            rect = rect.clip(surface.get_rect())
+            subsurf = surface.subsurface(rect)
+            # This is MUCH faster than draw.rect
+            subsurf.fill(back_color)
+
+        if fore_color:
+            pygame.draw.rect(surface, fore_color, rect, 2)
         
     def intersections(self, src, dest):
         # Not implemented.
