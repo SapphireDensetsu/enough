2e03b794ea6846fcaee818bf4b87b523c43c14eb
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-18 22:20:06 +0000

    Must commit before mv, svn sucks.

--- livec/gui/code/MetaWidget.py
+++ livec/gui/code/MetaWidget.py
@@ -1,21 +1,29 @@
-from gui.VBox import VBox
+from gui.VBox import HBox, VBox
 from gui.TextEdit import TextEdit
 import pygame
+from functools import partial
 
 class MetaWidget(VBox):
     def __init__(self, meta):
         self.meta = meta
         VBox.__init__(self, self.get_children)
-        self._cache = {}
+        self._hbox_cache = {}
+        self._vbox_cache = {}
 
     def _clean_cache(self):
-        for key, value in self._cache.iteritems():
+        for key, value in self._vbox_cache.iteritems():
             if key not in self.meta:
-                del self._cache[key]
+                self._vbox_cache.pop(key, None)
+                self._hbox_cache.pop(key, None)
 
     def get_children(self):
         self._clean_cache()
         for key, value in sorted(self.meta.iteritems()):
-            if key not in self._cache:
-                self._cache[key] = TextEdit(lambda : key)
-            yield self._cache[key]
+            if key not in self._vbox_cache:
+                self._vbox_cache[key] = HBox(partial(self._get_hbox_children, key, value))
+            yield self._vbox_cache[key]
+
+    def _get_hbox_children(self, key, value):
+        if key not in self._hbox_cache:
+            self._hbox_cache[key] = TextEdit(lambda : key), TextEdit(lambda : value)
+        return self._hbox_cache[key]
