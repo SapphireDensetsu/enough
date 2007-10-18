862618f43b8e7a529b43f4d2f2c4d8affc9d0b55
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-18 22:20:06 +0000

    Must commit before mv, svn sucks.

--- trunk/livec/gui/VBox.py
+++ trunk/livec/gui/VBox.py
@@ -1,18 +1,31 @@
 import pygame
 from gui.Widget import Widget
 
-class VBox(Widget):
+class Direction(object): pass
+
+class Horizontal(Direction): axis = 0
+class Vertical(Direction): axis = 1
+
+class Box(Widget):
     def __init__(self, get_children_widgets):
         self.get_children_widgets = get_children_widgets
     def draw(self, surface, pos):
-        x, y = pos
-        cx, cy = pos
-        mw = 0
+        pos = list(pos)
+        cur = list(pos)
+        max_len = 0
         for child in self.get_children_widgets():
-            w, h = child.draw(surface, (cx, cy))
-            mw = max(mw, w)
-            cy += h
-        tw, th = mw, cy-y
-        r = pygame.Rect((x, y), (tw-1, th-1))
+            size = child.draw(surface, cur)
+            max_len = max(max_len, size[self.direction.axis^1])
+            cur[self.direction.axis] += size[self.direction.axis]
+        total = [None, None]
+        total[self.direction.axis] = cur[self.direction.axis]-pos[self.direction.axis]
+        total[self.direction.axis^1] = max_len
+        r = pygame.Rect(pos, (total[0]-1, total[1]-1))
         pygame.draw.rect(surface, (255, 0, 0), r, 2)
-        return (tw, th)
+        return tuple(total)
+
+class VBox(Box):
+    direction = Vertical
+
+class HBox(Box):
+    direction = Horizontal
