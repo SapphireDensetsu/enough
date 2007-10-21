125d5d9ee8fe392ab45b6a4c215ddcd99d238ed6
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-21 16:26:49 +0000

    add tests

---
+++ trunk/livec/test/fix_import_paths.py
@@ -0,0 +1,4 @@
+import os
+import sys
+parent_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(0, parent_dir)
---
+++ trunk/livec/test/test_vbox.py
@@ -0,0 +1,20 @@
+from __future__ import with_statement
+import fix_import_paths
+from gui.Box import VBox
+from gui.TextEdit import TextEdit
+from gui.main import pygame_display
+from List import List
+
+def main():
+    with pygame_display((800, 600)) as display:
+        v = VBox(List([
+            TextEdit(lambda : 'Moshiko!'),
+            TextEdit(lambda : 'Dani!'),
+            TextEdit(lambda : 'Yosefa!'),
+            TextEdit(lambda : 'Yekutiel!'),
+        ]))
+        from gui.loop import loop
+        loop.loop(display.subsurface(10,10,500,500), v)
+
+if __name__ == '__main__':
+    main()
