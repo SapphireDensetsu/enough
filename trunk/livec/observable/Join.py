bcb97a5d8d27faa8669fdb361b6e565e90cfc142
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-22 03:04:12 +0000

    Oops forgot Join.py

---
+++ trunk/livec/observable/Join.py
@@ -0,0 +1,29 @@
+from observer import Observable
+
+class Join(Observable):
+    def __init__(self, separator_factory, l):
+        Observable.__init__(self)
+        self.separator_factory = separator_factory
+        self._cache = []
+        self.l = l
+        self.l.add_observer(self)
+        for item in self.l:
+            self._cache.append(item)
+            break
+        else:
+            return
+        for item in self.l:
+            self._cache.append(separator_factory())
+            self._cache.append(item)
+        
+    def observe_insert(self, list, index, item):
+        TODO
+    def observe_pop(self, list, index):
+        TODO
+
+from proxyclass import proxy_class
+Join = proxy_class(Join, '_cache', methods=[
+    '__getitem__',
+    '__len__',
+    'index',
+])
