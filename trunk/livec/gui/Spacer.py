c914497cb5ff8201a5b2eb84d7a857b9b6c1d96e
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-23 01:11:20 +0000

    Activation exists, not yet used

--- trunk/livec/gui/Spacer.py
+++ trunk/livec/gui/Spacer.py
@@ -4,7 +4,7 @@ class Spacer(Widget):
     def __init__(self, size):
         Widget.__init__(self)
         self._size = size
-    def draw(self, surface, pos):
+    def _draw(self, surface, pos):
         pass
     def size(self):
         return self._size
--- trunk/livec/gui/Widget.py
+++ trunk/livec/gui/Widget.py
@@ -1,9 +1,37 @@
+import pygame
 from Keymap import Keymap
 
 class Widget(object):
+    # frame_color is consulted first, and allowed to be None for no
+    # frame.
+    frame_color = None
+    frame_width = 2
+
     def __init__(self):
         self.keymap = Keymap()
+        self.keymap.add_observer(self)
+        self._prev_frame_color = None
+        
+    def observe_activated(self, keymap):
+        self._prev_frame_color = self.frame_color
+        self.frame_color = (255, 0, 0)
+
+    def observe_deactivated(self, keymap):
+        self.frame_color = self._prev_frame_color
+        self._prev_frame_color = None
+        
     def draw(self, surface, pos):
+        self.draw_frame(surface, pos)
+        self._draw(surface, pos)
+
+    def draw_frame(self, surface, pos):
+        size = self.size()
+        if self.frame_color is not None:
+            r = pygame.Rect(pos, size)
+            pygame.draw.rect(surface, self.frame_color, r, self.frame_width)
+
+    def _draw(self, surface, pos):
         raise NotImplementedError()
+
     def size(self):
         raise NotImplementedError()
