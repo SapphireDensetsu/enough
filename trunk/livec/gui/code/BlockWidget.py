ad090fdad24382cff461c418aa8ea1c650703f52
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-24 07:11:14 +0000

    Added semicolons

--- trunk/livec/gui/code/BlockWidget.py
+++ trunk/livec/gui/code/BlockWidget.py
@@ -4,20 +4,32 @@ from gui.Keymap import Key
 from lib.observable.List import List
 
 from gui.Box import VBox, HBox
-from gui.TextEdit import TextEdit
+from gui.TextEdit import make_label
 from gui.code.widget_for import widget_for, ccode_widget_for
 
 from lib.observable.CacheMap import CacheMap
+
+import ccode
+import style
 
 class BlockWidget(VBox):
     start_in_child = True
     def __init__(self, block):
         self.block = block
-        VBox.__init__(self, CacheMap(widget_for, self.block.statements))
+        VBox.__init__(self, CacheMap(self._widget_for, self.block.statements))
         
         self.keymap.register_keydown_noarg(Key(pygame.KMOD_CTRL, pygame.K_i),
                                            self._add_if)
 
+
+    def _widget_for(self, x):
+        w = widget_for(x)
+        if ccode.is_expr(x):
+            return HBox(List([
+                w,
+                make_label(style.semicolon, ';')
+            ]))
+        return w
 
     def _add_if(self):
         """Add a new 'if'"""
