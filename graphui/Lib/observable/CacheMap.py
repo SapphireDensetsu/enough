58d91ba7c16039c4ffcbc357ec3e82a1bffadec1
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-24 22:42:26 +0000

    graphui rewriting

---
+++ graphui/Lib/observable/CacheMap.py
@@ -0,0 +1,27 @@
+from lib.observer import Observable
+
+class CacheMap(object):
+    def __init__(self, func, l):
+        self.obs_list = Observable()
+        self.func = func
+        self._list = l
+        self._cache = map(self.func, self._list)
+        l.obs_list.add_observer(self, 'observe_')
+
+    def observe_insert(self, index, item):
+        new_item = self.func(item)
+        self._cache.insert(index, new_item)
+        self.obs_list.notify.insert(index, new_item)
+
+    def observe_pop(self, index):
+        # TODO: Should call destroy() here?
+        result = self._cache.pop(index)
+        self.obs_list.notify.pop(index)
+        return result
+
+from lib.proxyclass import proxy_class
+CacheMap = proxy_class(CacheMap, '_cache', methods=[
+    '__getitem__',
+    '__len__',
+    'index',
+])
