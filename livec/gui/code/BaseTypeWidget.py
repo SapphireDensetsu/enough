f4b14b45d6ea9c9bbae76c55385460fbc133b9b3
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-24 08:49:36 +0000

    Base types editable

--- livec/gui/code/BaseTypeWidget.py
+++
@@ -1,9 +0,0 @@
-from gui.TextEdit import TextEdit
-from gui.code.widget_for import widget_for
-
-import style
-
-class BaseTypeWidget(TextEdit):
-    def __init__(self, _type):
-        self.type = _type
-        TextEdit.__init__(self, style.base_type, lambda : self.type.name)
