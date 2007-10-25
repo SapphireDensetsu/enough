5f959230048487326f0c85eb03cb3852ae19dd46
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-25 18:53:15 +0000

    graphui - missing file

---
+++ trunk/graphui/MouseMap.py
@@ -0,0 +1,34 @@
+from Lib.Point import Point
+import pygame
+
+class MouseEvent(object):
+    def __init__(self, pygame_mouse_event):
+        self.type = pygame_mouse_event.type
+        self.pos = Point(pygame_mouse_event.pos)
+        
+class MouseArea(object):
+    def __init__(self, in_bounds):
+        self.in_bounds = in_bounds
+
+class MouseMap(object):
+    def __init__(self):
+        self.mouse_areas = []
+        self.area_to_handler = {}
+
+    def mouse_event(self, event):
+        p = event.pos
+        for mouse_area, handler in self.mouse_areas:
+            if mouse_area.in_bounds(p):
+                handler(event)
+
+    def push_area(self, mouse_area, handler):
+        assert mouse_area not in self.area_to_handler
+        self.area_to_handler[handler] = mouse_area
+        self.mouse_areas.insert(0, (mouse_area, handler))
+    def remove_area(self, area):
+        handler = self.area_to_handler[area]
+        self.mouse_areas.remove((area, handler))
+        del self.area_to_handler[area]
+                
+
+    
