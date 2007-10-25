88fe2362c9f473d7e3927d343946953f81e15260
lenoam <lenoam@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-25 20:48:16 +0000

    added missing graphui files

---
+++ trunk/graphui/style.py
@@ -0,0 +1,13 @@
+from TextEdit import TextEdit, TextStyle
+
+
+def _make_style(color=(220, 220, 220), font_size=16, font_name='fonts/FreeSansBold.ttf',
+                bgcolor=None, is_italic=False, is_underline=False, is_bold=False):
+    return TextStyle(color, font_size, font_name, bgcolor,
+                     is_italic, is_underline, is_bold)
+
+style = _make_style()
+
+key_name = _make_style(font_size=12, color=(80, 255, 80))
+key_space_width = 30
+keydoc_name = _make_style(font_size=12, color=(255, 255, 255))
