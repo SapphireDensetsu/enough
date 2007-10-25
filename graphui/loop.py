926b718d13e9e0ee17fd385a5f29b2cbe8bbfdc3
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-25 07:02:40 +0000

    Fix reactor loop

--- graphui/loop.py
+++ graphui/loop.py
@@ -1,15 +1,21 @@
 import pygame
 from Keymap import Keymap, Key
+from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
+import twisted.python.log
 
 import draw
 
 class ExitLoop(Exception): pass
 
 class Loop(object):
+    fps = 20
     def __init__(self):
         self.global_keymap = Keymap()
         self.global_keymap.register_key_noarg(Key(pygame.KMOD_CTRL, pygame.K_q),
                                               self._quit)
+        self.global_keymap.activate()
+        self.lc = LoopingCall(self._iteration)
         
     def _handle_event(self, event):
         if event.type == pygame.QUIT:
@@ -18,22 +24,24 @@ class Loop(object):
             self.global_keymap.key_event(event)
     def _quit(self):
         """Quits the program"""
-        raise ExitLoop()
+        self.lc.stop()
     def loop(self, display, widget):
-        self.global_keymap.activate()
-        self.global_keymap.set_next_keymap(widget.keymap)
-        c = pygame.time.Clock()
-        while True:
-            draw.offset.step()
-            c.tick(20)
-            for event in pygame.event.get():
-                try:
-                    self._handle_event(event)
-                except ExitLoop:
-                    return
-            draw.fill(display, (0, 0, 0))
-            widget.update()
-            widget.draw(display, (0, 0))
-            pygame.display.flip()
+        self.display = display
+        self.widget = widget
+        self.global_keymap.set_next_keymap(self.widget.keymap)
+
+        d = self.lc.start(1. / self.fps)
+        d.addCallback(lambda result: reactor.stop())
+        d.addErrback(twisted.python.log.err)
+        reactor.run()
+    
+    def _iteration(self):
+        draw.offset.step()
+        for event in pygame.event.get():
+            self._handle_event(event)
+        draw.fill(self.display, (0, 0, 0))
+        self.widget.update()
+        self.widget.draw(self.display, (0, 0))
+        pygame.display.flip()
 
 loop = Loop()
