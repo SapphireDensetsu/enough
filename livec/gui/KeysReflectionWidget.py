c08b37e5cd20d658f0f38b28f908316dcd1ad71d
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-26 01:10:10 +0000

    KeysReflectionWidget is a generic gui widget

--- livec/gui/KeysReflectionWidget.py
+++ livec/gui/KeysReflectionWidget.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2007 Enough Project.
-# See LICENSE for details.
-
 import pygame
 from gui.Box import HBox, VBox
 from gui.TextEdit import make_label
@@ -9,21 +6,22 @@ from lib.observable.CacheMap import CacheMap
 from lib.observable.List import List
 from lib.observable.SortedItems import SortedItems
 
-import style
-
 # TODO: Use a Table, not a vbox of hboxes
 class KeysReflectionWidget(VBox):
     selectable = False
     
-    def __init__(self, root):
+    def __init__(self, root, key_name_style, keydoc_name_style, key_space_width=30):
         self.root = root
+        self.key_name_style = key_name_style
+        self.keydoc_name_style = keydoc_name_style
+        self.key_space_width = key_space_width
         VBox.__init__(self, CacheMap(self._widget, SortedItems(self.root)))
 
     def _widget(self, (key, func)):
         from gui.Spacer import Spacer
         f = func.__doc__
         return HBox(List([
-            make_label(style.keydoc_name, f),
-            Spacer((style.key_space_width, 0)),
-            make_label(style.key_name, key.name()),
+            make_label(self.keydoc_name_style, f),
+            Spacer((self.key_space_width, 0)),
+            make_label(self.key_name_style, key.name()),
         ]))
