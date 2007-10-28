ff743b6e9aedd75ba3caceacd50b1579d315ee42
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-28 19:11:37 +0000

    cached is wrong

--- trunk/lib/FuncTools.py
+++ trunk/lib/FuncTools.py
@@ -1,17 +1,6 @@
 # Copyright (c) 2007 Enough Project.
 # See LICENSE for details.
 
-def cached(func):
-    results = {}
-    def new_func(*args, **kw):
-        if kw:
-            return func(*args, **kw)
-        
-        if args not in results:
-            results[args] = func(*args, **kw)
-        return results[args]
-    return new_func
-
 from functools import partial
 
 class PicklablePartial(object):
