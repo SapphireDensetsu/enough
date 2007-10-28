c821a57d9ef4a4ae861825582cab000a52e195d6
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-28 17:38:57 +0000

    Move gui/main.py to be shared

--- graphui/main.py
+++ graphui/main.py
@@ -6,23 +6,9 @@ from __future__ import with_statement
 import pygame
 import contextlib
 from gui import draw
+from gui.main import pygame_display
 
 from Lib.Graph import Node, Edge
-
-@contextlib.contextmanager
-def pygame_display(*args, **kw):
-    pygame.init()
-    try:
-        yield draw.set_mode(*args, **kw)
-    except:
-        import sys
-        sys.last_type, sys.last_value, sys.last_traceback = sys.exc_info()
-        import traceback
-        traceback.print_exc()
-        import pdb
-        pdb.pm()
-    finally:
-        pygame.quit()
 
 res = (800, 600)
 with pygame_display(res, pygame.DOUBLEBUF) as display:
