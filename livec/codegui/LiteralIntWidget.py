d2c1ac47e2dcfb717c7726f8d6647f0282d65429
eyal.lotem <eyal.lotem@a7bb79e7-0a29-0410-affc-e9d0bc870252>
2007-10-29 06:29:41 +0000

    Fix keymap bug.

--- livec/codegui/LiteralIntWidget.py
+++ livec/codegui/LiteralIntWidget.py
@@ -29,9 +29,10 @@ class LiteralIntWidget(LiteralWidget):
         self.keymap.register_key(self.dec_mode_key, Keymap.keydown_noarg(self._set_dec_mode))
         self.keymap.register_key(self.oct_mode_key, Keymap.keydown_noarg(self._set_oct_mode))
 
-        # TODO: there's a bug here, this registration is somehow
-        # swallowed by the digits group
-        self.text_edit.editing_keymap.register_key(self.negate_key, Keymap.keydown_noarg(self._negate))
+        self.text_edit.editing_keymap.register_key(
+            self.negate_key,
+            Keymap.keydown_noarg(self._negate)
+        )
 
     def _negate(self):
         """Negate the number"""
